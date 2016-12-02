36f60cb63bfe61a64d8e0a5e31603c8fcd74aaed
TobiahRex <tobiah.rex@gmail.com>
2016-12-01 22:14:36 -0800

    modified error message generation.

--- pythonRunner.js
+++ pythonRunner.js
@@ -1,9 +1,16 @@
 import cp from 'child_process';
-const spawn = cp.spawn('python', ['pyScripts/hello_world.py']);
+import path from 'path';
+import fs from 'fs';
+
+console.log(path.resolve('pyScripts/hello_world.py'));
+const spawn = cp.spawn('python', ['hello_world.py']);
 let string = '';
 let error = '';
 
 spawn.stdout.on('data', (data) => (string += data));
-spawn.stderr.on('data', (error) => console.log('stderr: ', error));
+spawn.stderr.on('data', (err) => {
+  error += err;
+  console.log('error: ', error)
+});
 
 spawn.on('close', (code) => console.log('string: ', string));
