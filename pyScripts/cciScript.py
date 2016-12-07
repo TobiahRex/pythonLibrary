b1e85d7b1efcbbfbe96f65163f739ee7b0ae895e
TobiahRex <tobiah.rex@gmail.com>
2016-12-06 21:57:38 -0800

    created cciStacks.py.

---
+++ pyScripts/cciScript.py
@@ -0,0 +1,14 @@
+def match(expression): # "def" is required when defining a function
+    ### This is called a 'docstring'.  It uses 3 pound signs on the outside.  You can think of it like JS's /**/ comment syntax. ###
+    stack = []
+    # data structures are declares using the "#" sign.
+
+    dict = {'(':')', '[':']', '{':'}'}
+    for x in expression:
+        if dict.get(x):
+            stack.append(dict[x])
+        else:
+            if len(stack) == 0 or x != stack[len(stack)-1]:
+                return False
+            stack.pop()
+    return len(stack) == 0
