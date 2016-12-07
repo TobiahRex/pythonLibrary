f691fe67a40949bbe3497cb0c220b611e0894ba0
TobiahRex <tobiah.rex@gmail.com>
2016-12-06 22:32:57 -0800

    added an example of a while loop.

--- pyScripts/loops.py
+++ pyScripts/loops.py
@@ -1,10 +1,19 @@
 # # ----------- for loop -----------
-# primes = [ 2, 3, 5, 7, 9 ]
-# for prime in primes:
-#     print prime
+primes = [ 2, 3, 5, 7, 9 ]
+for prime in primes:
+    print prime
 
 # ----------- for loop -----------
-xnumbers = [2, 3, 5, 7, 9]
+xnumbers = ['\n', 'yo', 'yo', 'this', 'is', 'a', 'python3', 'loop']
 for x in xnumbers:
      print x
 # this is going to throw an error for pyton 3 because xnumbers is not defined.
+
+# ----------- while loop -----------
+count = 0
+print '\n'
+while True:
+    print count
+    count += 1
+    if count >= 5:
+        break
