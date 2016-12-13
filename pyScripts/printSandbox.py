a4322d0477f303bf2fd087fe5462547d0ffa9a9f
TobiahRex <tobiah.rex@gmail.com>
2016-12-13 20:49:21 +0900

    added a curly brace type of string input.

--- pyScripts/printSandbox.py
+++ pyScripts/printSandbox.py
@@ -5,3 +5,7 @@ print("Total score for %s is %d" % (name, score))
 string1 = 'STRING 1'
 string2 = 'STRING 2'
 print("This is a sring: %s and another string: %s" % (string1, string2))
+# ------------------------------------------------------
+string3 = 'STRING 3'
+string4 = 'STRING 4'
+print("This is another example of a template string {} and another {}".format(string3, string4))
