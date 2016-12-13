8f35b7a62368f5faf3fbc3e39eb1855d4b2562e2
TobiahRex <tobiah.rex@gmail.com>
2016-12-13 20:47:34 +0900

    added string inside string

--- pyScripts/printSandbox.py
+++ pyScripts/printSandbox.py
@@ -1,3 +1,7 @@
 name = 'TobiahRex'
 score = 23
 print("Total score for %s is %d" % (name, score))
+# ------------------------------------------------------
+string1 = 'STRING 1'
+string2 = 'STRING 2'
+print("This is a sring: %s and another string: %s" % (string1, string2))
