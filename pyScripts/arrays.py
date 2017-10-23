30326c1e4d86cb42ef0ce6b01c850cab52b7d109
TobiahRex <tobiah.rex@gmail.com>
2017-10-22 17:06:22 -0700

    added "arrays.py".

--- pyScripts/arrays.py
+++ pyScripts/arrays.py
@@ -2,4 +2,5 @@
 list1 = ['one', 'two', 'three', 'four', 'five', 'six'];
 print('list = {}\n'.format(list1))
 print('list[:5]: {}\n'.format(list1[:5]))
-print('list[1::2]: {}'.format(list1[1::2]))
+print('list[1::2]: {}\n'.format(list1[1::2]))
+print('list1[:-5]: {}\n'.format(list1[:-5]))
