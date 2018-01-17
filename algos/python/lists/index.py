8f8e4f8b8b9579c939b225f16cdba16564921b68
TobiahRex <tobiah.rex@gmail.com>
2018-01-16 22:37:58 -0700

    created lists/

---
+++ algos/python/lists/index.py
@@ -0,0 +1,8 @@
+aList = [];
+aList.append('toby');
+aList.append('tobiah');
+aList.append('bickley');
+# NOTE Cannot chain method calls in Python.
+# Must call a new method on the returning result.
+
+print(aList.append['rex']);  # BUG TypeError: 'builtin_function_or_method' object is not subscriptable | IOW, cannot subscribe to the output of a method call.
