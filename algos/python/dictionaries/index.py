7e1d6e337395381afed96f04a473296ec7d56571
TobiahRex <tobiah.rex@gmail.com>
2018-01-16 22:44:46 -0700

    created a dictionary/.

---
+++ algos/python/dictionaries/index.py
@@ -0,0 +1,14 @@
+aDict = {
+         'toby': 28,
+         'justin': 37,
+         'chris': 60,
+        }; #{'toby': 28, 'justin': 37, 'chris': 60}
+
+aDict['toby'] = 1 #{'toby': 1, 'justin': 37, 'chris': 60}
+
+bDict = {
+ 'toby': 1,
+ 'justin': 37,
+}; #{'toby': 1, 'justin': 37}  #NOTE Violating the spacing still seems to work here. Interesting tho, undreadable.
+
+print(bDict);
