95cbcddeb2ef4349b62d483c5940f5bc364ae18e
TobiahRex <tobiah.rex@gmail.com>
2018-01-18 00:03:26 -0700

    successfully fetched story using urllib.request module.;

--- algos/python/loops/index.py
+++ algos/python/loops/index.py
@@ -1,3 +1,13 @@
+from urllib.request import urlopen;
+with urlopen('http://sixty-north.com/c/t.txt') as story:
+    story_words = [];
+    for line in story:
+        line_words = line.decode().split();
+        for word in line_words:
+            story_words.append(word);
+
+    print(story_words);
+
 obj = {
      'toby': 1,
      'tobiah': 2,
@@ -6,11 +16,7 @@ obj = {
     }
 # NOTE One cannot declare keys as implicit strings.  One must provide quotes around keys.
 
-for name in obj:
-    print(name, obj[name]);
-    # toby 1
-    # tobiah 2
-    # rex 3
-    # trex 4
+# for name in obj:
+    # print(name, obj[name]);
 
 # print(obj);
