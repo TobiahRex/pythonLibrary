8247f4dc834d76637a0cded2e15b4a613065281c
TobiahRex <tobiah.rex@gmail.com>
2018-01-17 23:42:22 -0700

    created an object to loop over.

--- algos/python/loops/index.py
+++ algos/python/loops/index.py
@@ -4,6 +4,9 @@ obj = {
      'rex': 3,
      'trex': 4,
     }
+# NOTE One cannot declare keys as implicit strings.  One must provide quotes around keys.
 
+for name in obj:
+    print(name, obj[name]);
 
-print(obj);
+# print(obj);
