1c0510a2ff71c09e14b528e3b4b6d7d837a791e1
TobiahRex <tobiah.rex@gmail.com>
2018-01-23 10:36:51 -0700

    created an exp2 function for python switch case substitute.

---
+++ algos/python/functions/index.py
@@ -0,0 +1,37 @@
+def exp1():
+    obj = {
+            'name': 'Toby',
+            'age': 28
+        }
+
+    def x():
+      return (obj, { 'name': 'Tobiah', 'age': 29 })
+
+    obj2, obj3 = x()
+    print('obj2: %s \nobj3: %s' % (obj2, obj3))
+
+def exp2():
+    list1 = []
+
+    def addTo(x):
+        print('adding...')
+        list1.append(x)
+
+    def remove(y):
+        print('removing...')
+        list1.pop()
+
+    def run(a, z):
+        switcher = {
+            'f1': addTo,
+            'f2': remove,
+            'f3': lambda: 'default',
+        }
+
+        func = switcher.get(a, lambda: 'nothing');
+        func(z);
+
+    run('f1', { 'name': 'Toby' });
+    print(list1)
+
+exp2()
