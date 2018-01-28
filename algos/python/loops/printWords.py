ce24c11a915e5194ae29029ca5e43d13c8923877
TobiahRex <tobiah.rex@gmail.com>
2018-01-28 14:50:05 -0700

    added complete docString to all module level functions.

--- algos/python/loops/printWords.py
+++ algos/python/loops/printWords.py
@@ -7,6 +7,14 @@ import sys
 from urllib.request import urlopen
 
 def fetch(url):                                 #'http://sixty-north.com/c/t.txt'
+    '''Fetch text document from <URL>
+
+    Args:
+        url: The URL of a UTF-8 text document.
+
+    Return:
+        The fetch response as a list of words.
+    '''
     with urlopen(url) as story:
         story_words = []
         for line in story:
@@ -17,10 +25,20 @@ def fetch(url):                                 #'http://sixty-north.com/c/t.txt
 
 
 def print_words(items):
+    '''Print list of words as a single spaced, single line string.
+
+    Args:
+        items: The list of word items to print.
+    '''
     print(' '.join(items))
 
 
 def main(url):
+    '''Invoked directly as a script; Makes a composite call to fetch() and passes result to print().
+
+    Args:
+        url: The URL of a UTF-8 text document.
+    '''
     words = fetch(url)
     print_words(words)
 
