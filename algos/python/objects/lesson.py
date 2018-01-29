0cd8b7a8a935cf7e16b660481438af434f2350ab
TobiahRex <tobiah.rex@gmail.com>
2018-01-29 15:50:22 -0700

    building out a "printBreak" function as a title.

--- algos/python/objects/lesson.py
+++ algos/python/objects/lesson.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 m = [ 5, 3, 8, 2]
-def updateList(m):
-    m.append(1)
-    print('list: ', m)
 
-print(m)
+def printBreak(title, lineBreak = '-'):
+    for break in len(title):
+        print(lineBreak);
+        print(title)
+        print(lineBreak)
+
+def updateList(k):
+    k.append(1)
+    print('list: ', k)
+    print('original list: ', m)
+
+
+def updateList2(a):
+    a = m
+    a.append(1)
+    print('list2: ', a)
+    print('original list: ', m)
 
 if __name__ == '__main__':
     updateList(m)
+    print()
+    updateList2(m)
