968dcac95092b9a2678d37a5acb418a92fece478
TobiahRex <tobiah.rex@gmail.com>
2018-01-29 19:01:27 -0700

    created a globalVar expirement.

--- algos/python/objects/scopes.py
+++ algos/python/objects/scopes.py
@@ -13,22 +13,43 @@ def localScope():
     x = 'This is a locally scoped variable - scoped by the function block' # Technically this line is "lexically scoped" - You can think of "lexical" scoping as the result of assignments or bindings.
     print('x: ', x)
 
-def enclosedScope_outter():
-    y = 'This is local scope of "enclosedScope_outter" BUT, the enclosed scope of "enclosedScope_inner"'
-    print('y (outter) 1: ', y)  #y (outter) 1:  This is local scope of "enclosedScope_outter" BUT, the enclosed scope of "enclosedScope_inner"
+def outter():
+    y = 'This is local scope of "enclosedScope_outter" BUT, the enclosed scope of "inner"'
+    print('y (outter) 1: ', y)  #y (outter) 1:  This is local scope of "enclosedScope_outter" BUT, the enclosed scope of "inner"
 
 
-    def enclosedScope_inner():
-        z = 'This is the local scope of "enclosedScope_inner".'
+    def inner():
+        z = 'This is the local scope of "inner".'
         y = 'This is the Y re-assignment'
         print('y (inner): ', y)  #y (inner):  This is the Y re-assignment
 
-    enclosedScope_inner()
+    inner()
 
-    print('y (outter) 2:', y)  #y (outter) 2: This is local scope of "enclosedScope_outter" BUT, the enclosed scope of "enclosedScope_inner"
+    print('y (outter) 2:', y)  #y (outter) 2: This is local scope of "enclosedScope_outter" BUT, the enclosed scope of "inner"
 
     '''
-        The last print - "y (outter) 2" is still the original value even after assigned y in the inner function.
+        The last print - "y (outter) 2" is still the original value even after assigned y in the "inner" function.
         This is because the assignment happened inside an "enclosed" scope...
-        Therefore "y" was a completely different object referenc than the "y" in the outter function.
+        Therefore "y" was a completely different object reference than the "y" in the "outter" function.
     '''
+
+globalVar = 3
+
+def redefine():
+    globalVar = 5
+
+redefine()
+print(globalVar) # 3
+'''
+    The redefinition of "globalVar" inside "redefine" does not change the global variable "globalVar" in the module scope.
+    Instead, a NEW "globalVar" object reference is created within the Local Scope of the "redefine()" function.
+
+    To re-assign the global variable "globalVar" from within a locally scoped function, the following must be done...
+'''
+
+def redefine2():
+    global globalVar
+    globaVar = 5
+
+redefine2()
+print(globalVar)
