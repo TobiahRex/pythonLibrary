b53c309fa2cff7828353639d7fc68cb1bcc8131c
TobiahRex <tobiah.rex@gmail.com>
2018-01-29 18:32:28 -0700

    added notes for types.py.

--- algos/python/objects/types.py
+++ algos/python/objects/types.py
@@ -4,5 +4,19 @@
     Dynamically typed languages, are so because they are not exectuded until the program that they are defining is run.
     In contrast, languages like Java, C, C++ all run their programs BEFORE they are actually executed.  Hence their Strict type system.
 
-    Python also allows for type's to be IMPLICITLY altered when combining certain types.
+    Python does NOT allow for type's to be IMPLICITLY altered when combining certain types - hence "STRONG".
+    ("if" statements and "while" loop predicates are converted to "Bool" type for truthy or falsey evals).
+    The requirement for this to take place is that when calling certain global/module methods with certain types,
+    that type must have a defined support for that method.
 '''
+def add(a, b):
+    result = a + b
+    print(result)
+
+result = add('tobiah', 'rex')
+
+result = add(1.230, 1.707)
+
+result = add([1, 3], [2, 4])
+
+# add('Tobiah', 29)
