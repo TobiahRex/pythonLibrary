9be0cf070075ce5fd99cc93572d678780a0fe12d
TobiahRex <tobiah.rex@gmail.com>
2018-01-29 18:22:16 -0700

    finished sentinel.py.

--- algos/python/objects/sentinel.py
+++ algos/python/objects/sentinel.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 from time import ctime
 
+
+'''
+    At module runtime, the function times() is defined, and the default argument is assigned the value [] at that moment.
+    Later, each occurence of invocation appends to the same list.
+    It does NOT re-defined the default argument as an empty list as may be expected.
+'''
 def times(t = []):
     t.append(ctime())
     print('t: ', t);
 
+
+'''
+    At module runtime, the function addSpam() is defined and the default argument 'food' is defined as None.
+'''
 def addSpam(food = None):
     if food is None:
         food = []
---
+++ algos/python/objects/types.py
@@ -0,0 +1,8 @@
+'''
+    Python is a DYNAMIC & STRONG type system.
+
+    Dynamically typed languages, are so because they are not exectuded until the program that they are defining is run.
+    In contrast, languages like Java, C, C++ all run their programs BEFORE they are actually executed.  Hence their Strict type system.
+
+    Python also allows for type's to be IMPLICITLY altered when combining certain types.
+'''
